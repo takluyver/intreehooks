02c0f94860a69d6ce20ef8c7da16aa914f73e9ea
Thomas Kluyver <thomas@kluyver.me.uk>
2017-12-01 16:11:44 +0000

    Note about test backend module

--- examples/good/sample_backend.py
+++ examples/good/sample_backend.py
@@ -1,2 +1,5 @@
+# A proper backend needs more hooks than this - this is just to test resolving
+# it through intreehooks.
+
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     return 'itsalie'
